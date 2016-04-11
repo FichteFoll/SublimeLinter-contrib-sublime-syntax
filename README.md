69107161dacdab02c81bd6a63e474396bde43cb9
FichteFoll <fichtefoll2@googlemail.com>
2016-04-11 05:13:31 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+SublimeLinter-contrib-sublime-syntax
+================================
+
+<!-- [![Build Status](https://travis-ci.org/SublimeLinter/SublimeLinter-contrib-sublime-syntax.svg?branch=master)](https://travis-ci.org/SublimeLinter/SublimeLinter-contrib-sublime-syntax) -->
+
+This linter plugin for [SublimeLinter][docs] provides an interface to Sublime Text's internal syntax testing engine. It will be used on files that define syntax tests for Sublime Text (i.e. files starting with `syntax_test`).
+
+## Installation
+SublimeLinter 3 must be installed in order to use this plugin. If SublimeLinter 3 is not installed, please follow the instructions [here][installation].
+
+### Plugin installation
+Please use [Package Control][pc] to install the linter plugin. This will ensure that the plugin will be updated when new versions are available. If you want to install from source so you can modify the source code, you probably know what you are doing so we won’t cover that here.
+
+**You must be using Sublime Text Build 3092 or higher in order to install and use this linter.**
+
+To install via Package Control, do the following:
+
+1. Within Sublime Text, bring up the [Command Palette][cmd] and type `install`. Among the commands you should see `Package Control: Install Package`. If that command is not highlighted, use the keyboard or mouse to select it. There will be a pause of a few seconds while Package Control fetches the list of available plugins.
+
+1. When the plugin list appears, type `sublime-syntax`. Among the entries you should see `SublimeLinter-contrib-sublime-syntax`. If that entry is not highlighted, use the keyboard or mouse to select it.
+
+## Settings
+For general information on how SublimeLinter works with settings, please see [Settings][settings]. For information on generic linter settings, please see [Linter Settings][linter-settings].
+
+
+## Contributing
+If you would like to contribute enhancements or fixes, please do the following:
+
+1. Fork the plugin repository.
+1. Hack on a separate topic branch created from the latest `master`.
+1. Commit and push the topic branch.
+1. Make a pull request.
+1. Be patient.  ;-)
+
+Please note that modifications should follow these coding guidelines:
+
+- Indent is 4 spaces.
+- Code should pass flake8 and pep257 linters.
+- Vertical whitespace helps readability, don’t be afraid to use it.
+- Please use descriptive variable names, no abbreviations unless they are very well known.
+
+Thank you for helping out!
+
+[docs]: http://sublimelinter.readthedocs.org
+[installation]: http://sublimelinter.readthedocs.org/en/latest/installation.html
+[locating-executables]: http://sublimelinter.readthedocs.org/en/latest/usage.html#how-linter-executables-are-located
+[pc]: https://packagecontrol.io/installation
+[cmd]: http://docs.sublimetext.info/en/sublime-text-3/extensibility/command_palette.html
+[settings]: http://sublimelinter.readthedocs.org/en/latest/settings.html
+[linter-settings]: http://sublimelinter.readthedocs.org/en/latest/linter_settings.html
+[inline-settings]: http://sublimelinter.readthedocs.org/en/latest/settings.html#inline-settings
---
+++ linter.py
@@ -0,0 +1,152 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by FichteFoll
+# Copyright (c) 2016 FichteFoll
+#
+# License: MIT
+#
+
+"""This module exports the SublimeSyntax plugin class."""
+
+import contextlib
+import re
+import os.path
+
+import sublime
+import sublime_api
+
+from SublimeLinter.lint import Linter, persist
+
+
+class SublimeSyntax(Linter):
+
+    """Provides an interface to sublime-syntax.
+
+    This linter uses Sublime Text's internal sublime_api module
+    to access the syntax test running code.
+
+    Because that has rather harsh requirements,
+    we rely on creating temporary files in the Packages folder
+    in order to provide the full functionality.
+    """
+
+    # Must be active on all syntaxes,
+    # so we can check view's eligibility in the `run` method.
+    syntax = '*'
+    cmd = None
+    regex = (
+        r'^[^:]+:(?P<line>\d+):((?P<col>\d+):)? '
+        r'(?P<message>.+)'
+    )
+    word_re = r'.'  # only highlight a single character
+
+    def run(self, cmd, code):
+        """Check if file is 'lintable' and perform linting."""
+        # It has to start with `"syntax_test"`
+        # and has a specific first line, technically.
+        # We only require one of each
+        # (to also lint unsaved views).
+        basename = os.path.basename(self.filename)
+        if not basename or not basename.startswith("syntax_test"):
+            # This actually gets reported by the test runner,
+            # so we only check for an additionally qualifying file
+            # if the filename check fails.
+            first_line = code[:code.find("\n")]
+            match = re.match(r'^(\S*) SYNTAX TEST "([^"]*)"', first_line)
+            if not match:
+                return
+
+        # The syntax test runner only operates on resource files that the resource loader can load,
+        # which must reside in a "Packages" folder
+        # and has the restriction of only working on saved files.
+        # Instead, we create a temporary file somewhere in the packages folder
+        # and pass that.
+        with _temporary_resource_file(code, prefix="syntax_test_") as resource_path:
+            assertions, test_output_lines = sublime_api.run_syntax_test(resource_path)
+
+        output = "\n".join(test_output_lines)
+        if persist.debug_mode():
+            persist.printf('{}: "{}" assertions: {}'.format(self.name, basename, assertions))
+            persist.printf('{}: "{}" output: \n  {}'.format(self.name, basename,
+                                                            "\n  ".join(test_output_lines)))
+
+        return output
+
+
+##################################################
+# Utility for temporary resource files
+
+_temp_dir_name = ".temp-subsyn-lint"
+_temp_path = None
+
+
+def plugin_loaded():
+    """Build and remove temporary path."""
+    # Required for sublime.packages_path().
+    # ST only "loads resources" from the Packages dir.
+    global _temp_path
+    packages_path = sublime.packages_path()
+    _temp_path = os.path.join(packages_path, _temp_dir_name)
+
+    _remove_temp_path()
+
+
+def plugin_unloaded():
+    """Remove temporary path."""
+    _remove_temp_path()
+
+
+def _remove_temp_path():
+    """Try to clean our temp dir if it exists."""
+    if os.path.exists(_temp_path):
+        def onerror(function, path, excinfo):
+            persist.printf("{}: Unable to delete '{}' while cleaning up temporary directory"
+                           .format(SublimeSyntax.name, path))
+            import traceback
+            traceback.print_exc(*excinfo)
+        import shutil
+        shutil.rmtree(_temp_path, onerror=onerror)
+
+
+@contextlib.contextmanager
+def _temporary_resource_file(text, prefix='', suffix=''):
+    """Create a temporary file in ST's "resource" folder, using tempfile.mkstemp.
+
+    Yields the relative resource path as a context manager
+    and removes it when the scope is exited.
+
+    Files are stored in a Temp folder relative to the Data folder,
+    which is removed afterwards if it does not contain any other files.
+    """
+    import tempfile
+
+    # Ensure the folder exists
+    if not os.path.exists(_temp_path):
+        os.mkdir(_temp_path)
+
+    try:
+        fd, temp_file_path = tempfile.mkstemp(prefix=prefix, suffix=suffix, dir=_temp_path)
+        if persist.debug_mode():
+            persist.printf("{}: created temporary file at {}"
+                           .format(SublimeSyntax.name, temp_file_path))
+
+        try:
+            with open(fd, 'w') as f:
+                f.write(text)
+            temp_file_resource_path = "/".join(["Packages", _temp_dir_name,
+                                                os.path.basename(temp_file_path)])
+            yield temp_file_resource_path
+        finally:
+            os.remove(temp_file_path)
+
+    finally:
+        # And remove the folder, if it's empty.
+        # Otherwise wait for a "restart".
+        try:
+            os.rmdir(_temp_path)
+        except OSError as e:
+            if persist.debug_mode():
+                persist.printf("{}: unable to delete temporary folder; {}"
+                               .format(SublimeSyntax.name, e))
